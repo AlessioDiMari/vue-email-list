14370ef535666a66545e604c7d1cd4c8c086512a
AlessioDiMari <alessiodimari.adm25@gmail.com>
2024-02-28 03:17:20 +0100

    html e push tramite API

--- js/script.js
+++ js/script.js
@@ -3,8 +3,20 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
+            emails: [
 
+            ]
         }
     },
-    
+
+    mounted() {
+        // utilizzando un api pusho la email generata randomicamente nel'array emails
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then(
+            (esito) => {
+                console.log(esito.data.response);
+                this.emails.push(esito.data.response)
+            }
+        )
+    },
+
 }).mount("#app");

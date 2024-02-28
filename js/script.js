69be75bc5830b43e640a9839ff31818d07640671
AlessioDiMari <alessiodimari.adm25@gmail.com>
2024-02-28 03:25:54 +0100

    ciclo for per popolare l'array

--- js/script.js
+++ js/script.js
@@ -10,13 +10,18 @@ createApp({
     },
 
     mounted() {
-        // utilizzando un api pusho la email generata randomicamente nel'array emails
-        axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then(
-            (esito) => {
-                console.log(esito.data.response);
-                this.emails.push(esito.data.response)
-            }
-        )
+
+        // Creo un ciclo for per far riempire l'array 10 volte
+        for(let i = 0; i < 10; i++){
+            // utilizzando un api pusho la email generata randomicamente nel'array emails
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then(
+                (esito) => {
+                    console.log(esito.data.response);
+                    this.emails.push(esito.data.response)
+                }
+            )
+        }
+
     },
 
 }).mount("#app");
